9f0ee3d1122a43f9d8ec7b4c89c31619e1aff437
Grace <gracekolago21@gmail.com>
2023-07-18 03:58:38 -0700

    comit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # php-day3-2023
+Show data base
---
+++ conn.php
@@ -0,0 +1,9 @@
+<?php 
+$serverName = 'localhost';
+$userName = 'root';
+$password = '';
+$database = 'day10';
+
+$con =mysqli_connect($serverName,$userName,$password,$database);
+
+?>
